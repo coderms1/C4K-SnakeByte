84dab8a3651f0cba3c34b97db872880999832040
MS1 <157831131+coderms1@users.noreply.github.com>
2026-07-28 14:31:46 -0400

    Fix turtle shape registration and cleanup code

--- turtleGame.py
+++ turtleGame.py
@@ -1,16 +1,23 @@
 import turtle
-funkyT = turtle.Turtle()
-funkyT = turtle.Shape("funkyT.gif")
-def moveForward():
-    funkyT.forward(20)
-def turnLeft():
-    funkyT.left(30)
-def turnRight():
-    funkyT.right(30)
+
 screen = turtle.Screen()
 screen.register_shape("funkyT.gif")
+
+funkyT = turtle.Turtle()
+funkyT.shape("funkyT.gif")
+
+def moveForward():
+    funkyT.forward(20)
+
+def turnLeft():
+    funkyT.left(30)
+
+def turnRight():
+    funkyT.right(30)
+
 screen.onkey(moveForward, "Up")
 screen.onkey(turnLeft, "Left")
 screen.onkey(turnRight, "Right")
+
 screen.listen()
 turtle.done()
