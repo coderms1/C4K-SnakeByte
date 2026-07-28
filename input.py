cd42a5c2b8da19de15363add6d03d99356162846
MS1 <157831131+coderms1@users.noreply.github.com>
2026-07-28 10:51:29 -0400

    Add user input for name and age

---
+++ input.py
@@ -0,0 +1,5 @@
+# Input (prompt user for name & age)
+sName = input("Enter name: ")
+userAge = input("Enter age: ")
+# Output (display name & age for user)
+print(f"Hello, {sName}. You are {userAge} years old!")
