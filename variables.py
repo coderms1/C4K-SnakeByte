08621deaf792e0aa78ab5c7749de5a3e786e2ffe
MS1 <157831131+coderms1@users.noreply.github.com>
2026-07-28 10:51:07 -0400

    Define variables for string, int, float, and boolean
    Added variables of different types and a print statement.

---
+++ variables.py
@@ -0,0 +1,14 @@
+# string (words & symbols)
+sName = "Mr. Dude"
+# integers (whole numbers)
+iAge = 37
+print(f"I am {iAge} years old.")
+# float (number with decimals)
+fGpa = 3.98
+# boolean = True / False
+bIsTeacher = True
+
+"""
+This is a comment block.
+I hope you're having fun.
+"""
